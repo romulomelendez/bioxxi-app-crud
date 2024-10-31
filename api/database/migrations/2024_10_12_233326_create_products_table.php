cbf2254d292e300335f6b8f4e7e277832775f71f
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2024-10-31 19:10:26 -0300

    feat: first api endpoint is working

---
+++ api/database/migrations/2024_10_12_233326_create_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('type', ['crítico', 'semi-crítico', 'não-crítico']);
+            $table->text('description')->nullable();
+            $table->timestamp('inclusion')->useCurrent();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
