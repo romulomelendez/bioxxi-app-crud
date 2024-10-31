6b13f18a76e9c1e309f9d33fcb9ed9130067782c
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2024-10-31 19:41:27 -0300

    feat: get one specific product endpoint implemented

--- api/app/Http/Controllers/Api/ProductController.php
+++ api/app/Http/Controllers/Api/ProductController.php
@@ -38,9 +38,9 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
-    {
-        //
+    public function show(string $id) {
+        return product::findOrFail($id);
+        
     }
 
     /**
